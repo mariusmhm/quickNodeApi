8d48d5b5968168ba53c8a061cd3596f7dfc6c639
Marius <73521657+mariusmhm@users.noreply.github.com>
2024-07-09 11:43:51 +0200

    changed to express

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-import http from "http";
 
-// Create a server object
-const server = http.createServer((req, res) => {
-  setTimeout(() => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    // Write some text to the response
-    res.end("Welcome to my simple Node.js app!");
-  }, 15000);
-  // Set the response header
-});
+const express = require("express")
+
+const app = express ();
+app.use(express.json());
+
+
+app.get("/longresponse", (request, response) => {
+    setTimeout(() => {
+        response.send({info: "this should take 15 seconds"})
+    }, 15000)
+ });
+
 
 // Define the port to listen on
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Start the server
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
