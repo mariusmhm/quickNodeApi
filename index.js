955a69006455790193c13136dabfdf1730584ca4
Marius <73521657+mariusmhm@users.noreply.github.com>
2024-07-09 11:29:04 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import http from "http";
+
+// Create a server object
+const server = http.createServer((req, res) => {
+  setTimeout(() => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    // Write some text to the response
+    res.end("Welcome to my simple Node.js app!");
+  }, 15000);
+  // Set the response header
+});
+
+// Define the port to listen on
+const port = 3000;
+
+// Start the server
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
